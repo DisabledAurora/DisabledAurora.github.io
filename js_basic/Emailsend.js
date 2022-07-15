5157c2469e1183e258737fa218dc104c8e3e68ee
Disabled Jinx <47773944+DisabledAurora@users.noreply.github.com>
2022-07-15 16:02:38 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   js_basic/Emailsend.js
    	new file:   js_basic/buttonalert.js
    	modified:   style/index.css
    	modified:   style/index.scss

--- js_basic/Emailsend.js
+++ js_basic/Emailsend.js
@@ -8,20 +8,23 @@ function sendMail(params) {
     email: document.getElementById("Email").value,
     message: document.getElementById("msg").value,
   };
+
+  var ye = document.getElementById("success");
+  var nah = document.getElementById("failure");
+
   if (tempv.name != "" && tempv.email != "") {
     emailjs
       .send("service_svyxp0d", "template_p1xdv1g", tempv)
       .then((result) => {
-        alert("Successfully sent!");
+        ye.classList.remove("nodisplaynow");
 
         const inputs = document.querySelectorAll("#Name, #msg, #Email");
-
         inputs.forEach((input) => {
           input.value = "";
         });
       })
       .catch((e) => {
-        alert("Request was not submitted! Try again later");
+        nah.classList.remove("nodisplaynow");
       });
   }
 }
